4b40c3673928ac66834e8384cffd33bc3cde16cf
VadimVanak <148138618+VadimVanak@users.noreply.github.com>
2025-03-01 14:13:26 +0100

    Installation script for VM

---
+++ install.sh
@@ -0,0 +1,37 @@
+export DNS_NAME=cv1.vanak.de
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+# Check if certbot is installed and install it if not
+if ! command -v certbot &> /dev/null
+then
+    echo "Certbot not found. Installing..."
+    sudo apt update && sudo apt install -y certbot
+fi
+
+# Request DNS certificates
+sudo certbot certonly --standalone \
+    -d "$DNS_NAME" \
+    --register-unsafely-without-email \
+    --agree-tos 
+	
+apt-get install git
+git clone https://github.com/VadimVanak/E115_LaTeXResumeAI.git
+
+#TODO: update DNS name in docker project
+#TODO: run docker-compose
